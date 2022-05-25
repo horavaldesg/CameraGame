39555e9b2f974732b4119d483b4ddd6cca8389fd
Horacio Valdes <horavaldesg@gmail.com>
2022-05-25 14:47:08 -0500

    Initial Push

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    public static PlayerMovement controls;
+    public static bool canRotate;
+
+    Vector2 move;
+    public CharacterController cc;
+
+
+    public float speed = 5f;
+    public float verticalSpeed = 0;
+
+    public float Gravity = -9.8f;
+   
+
+    float rotY;
+    Vector3 movement;
+
+    Vector2 rotate;
+
+    private void Awake()
+    {
+        controls = new PlayerMovement();
+
+        controls.Player.Move.performed += tgb => move = tgb.ReadValue<Vector2>();
+        controls.Player.Move.canceled += tgb => move = Vector2.zero;
+        controls.Player.Look.performed += tgb => rotate = tgb.ReadValue<Vector2>();
+        controls.Player.Look.canceled += tgb => rotate = Vector2.zero;
+
+
+    }
+    private void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+    private void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+        canRotate = true;
+        cc = GetComponent<CharacterController>();
+
+    }
+   
+    void FixedUpdate()
+    {
+        //Movement
+        movement = Vector3.zero;
+
+
+        float xSpeed = move.y * speed * Time.deltaTime;
+        movement += transform.forward * xSpeed;
+        float ySpeed = move.x * speed * Time.deltaTime;
+        movement += transform.right * ySpeed;
+
+        //Gravtity
+        verticalSpeed += Gravity * Time.deltaTime;
+
+        movement += transform.up * verticalSpeed * Time.deltaTime;
+
+
+        /*
+        //Player Rotation
+        if (canRotate)
+        {
+            Vector2 r = new Vector2(0, rotate.x) * mouseSensitivity * Time.deltaTime * 10;
+            transform.Rotate(r, Space.Self);
+            Quaternion q = transform.rotation;
+            q.eulerAngles = new Vector3(q.eulerAngles.x, q.eulerAngles.y, 0);
+            transform.rotation = q;
+
+            //Camera Rotation
+
+            rotY += -rotate.y * mouseSensitivity * Time.deltaTime * 10;
+            rotY = Mathf.Clamp(rotY, -90, 90);
+            camTransform.transform.localRotation = Quaternion.Euler(rotY, 0, 0);
+        }*/
+        cc.Move(movement);
+
+
+    }
+}
---
+++ Assets/WebCam.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class WebCam : MonoBehaviour
+{
+    WebCamTexture webCamTexture;
+    [SerializeField]RawImage playerFace;
+    string selectedCam;
+    string frontFacing = " ";
+    string backFacing = " ";
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        
+        for(int i = 0; i < devices.Length; i++)
+        {
+            if (devices[i].isFrontFacing)
+            {
+                frontFacing = devices[i].name;
+            }
+            else
+            {
+                backFacing = devices[i].name;
+            }
+        }
+        Debug.Log(frontFacing + "/n" + backFacing);
+        selectedCam = frontFacing;
+        StartCamera();
+    }
+    private void StartCamera()
+    {
+        webCamTexture = new WebCamTexture(selectedCam);
+        GetComponent<Renderer>().material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+    }
+    private void StopCam()
+    {
+        webCamTexture.Stop();
+    }
+    public void TakePhoto()
+    {
+        StartCoroutine(Capture());
+    }
+    IEnumerator Capture()
+    {
+        yield return new WaitForEndOfFrame();
+        Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+        photo.SetPixels(webCamTexture.GetPixels());
+        photo.Apply();
+        playerFace.texture = photo;
+        gameObject.SetActive(false);
+    }
+    public void FlipCamera()
+    {
+        if(selectedCam == frontFacing)
+        {
+            selectedCam = backFacing;
+            StopCam();
+            StartCamera();
+        }
+        else if (selectedCam == backFacing)
+        {
+            selectedCam = frontFacing;
+            StopCam();
+            StartCamera();
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
